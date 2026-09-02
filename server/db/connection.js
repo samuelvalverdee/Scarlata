143078545ccacdd30fd8dc15fb4336480ed2d401
samuel valverde <samuel.valverde.2802@gmail.com>
2026-09-02 12:50:09 -0600

    chore: reset main to README-only
    main will stay minimal until backend and frontend are consolidated
    and ready to ship; develop is the working branch where feature
    branches merge in the meantime.

--- server/db/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB(MONGO_URI = 'mongodb://127.0.0.1:27017/scarlata') {
-    await mongoose.connect(MONGO_URI);
-}
-
-module.exports = connectDB;
